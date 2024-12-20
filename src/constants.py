3a6bec2e6b713275f02e6eb05341770ac25d39c1
Chidam Gopal <cgopal@mozilla.com>
2024-12-20 17:44:47 -0500

    user history generation in progress

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,13 @@
 # embedding models dictionary
 EMBEDDING_MODELS_DICT = {
     "Xenova/all-MiniLM-L6-v2": "https://huggingface.co/Xenova/all-MiniLM-L6-v2/resolve/main/onnx/model_quantized.onnx",
-    "Xenova/distiluse-base-multilingual-cased-v1": "https://huggingface.co/Xenova/distiluse-base-multilingual-cased-v1/resolve/main/onnx/model_quantized.onnx",
-    "Xenova/distiluse-base-multilingual-cased-v2": "https://huggingface.co/Xenova/distiluse-base-multilingual-cased-v2/resolve/main/onnx/model_quantized.onnx",
-    "Xenova/paraphrase-multilingual-MiniLM-L12-v2": "https://huggingface.co/Xenova/paraphrase-multilingual-MiniLM-L12-v2/resolve/main/onnx/model_quantized.onnx",
-    "Xenova/paraphrase-multilingual-mpnet-base-v2": "https://huggingface.co/Xenova/paraphrase-multilingual-mpnet-base-v2/resolve/main/onnx/model_quantized.onnx",
     "nomic-ai/nomic-embed-text-v1.5": "https://huggingface.co/nomic-ai/nomic-embed-text-v1.5/resolve/main/onnx/model_quantized.onnx",
-    "Xenova/LaBSE": "https://huggingface.co/Xenova/LaBSE/resolve/main/onnx/model_quantized.onnx",
+    "Xenova/all-mpnet-base-v2": "https://huggingface.co/Xenova/all-mpnet-base-v2/resolve/main/onnx/model_quantized.onnx",
+    "Xenova/paraphrase-mpnet-base-v2": "https://huggingface.co/Xenova/paraphrase-mpnet-base-v2/resolve/main/onnx/model_quantized.onnx",
+    "Xenova/all-MiniLM-L12-v2": "https://huggingface.co/Xenova/all-MiniLM-L12-v2/resolve/main/onnx/model_quantized.onnx",
+    # "Xenova/distiluse-base-multilingual-cased-v1": "https://huggingface.co/Xenova/distiluse-base-multilingual-cased-v1/resolve/main/onnx/model_quantized.onnx",
+    # "Xenova/distiluse-base-multilingual-cased-v2": "https://huggingface.co/Xenova/distiluse-base-multilingual-cased-v2/resolve/main/onnx/model_quantized.onnx",
+    # "Xenova/paraphrase-multilingual-MiniLM-L12-v2": "https://huggingface.co/Xenova/paraphrase-multilingual-MiniLM-L12-v2/resolve/main/onnx/model_quantized.onnx",
+    # "Xenova/paraphrase-multilingual-mpnet-base-v2": "https://huggingface.co/Xenova/paraphrase-multilingual-mpnet-base-v2/resolve/main/onnx/model_quantized.onnx",
+    # "Xenova/LaBSE": "https://huggingface.co/Xenova/LaBSE/resolve/main/onnx/model_quantized.onnx",
 }
